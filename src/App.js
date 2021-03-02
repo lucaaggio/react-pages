3fd8180525db0a1a4e5bb9e86aadd75f000eca3c
Luca Aggio <lucaaggio.studia@mohole.it>
2021-03-02 23:51:22 +0100

    add .env

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+const message = process.env.REACT_APP_MESSAGE || "Manca un dato.."
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p> 
+        {message}
         </p>
         <a
           className="App-link"
@@ -19,6 +21,7 @@ function App() {
         </a>
       </header>
     </div>
+    
   );
 }
 
